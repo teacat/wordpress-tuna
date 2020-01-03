b05e456d3fb66630dab35578712a032e510b584c
YamiOdymel <yamiodymel@gmail.com>
2020-01-03 17:02:04 +0800

    push

---
+++ singular.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Document</title>
+</head>
+<body class="homepage">
+    <!-- 導航欄 -->
+    <nav>
+
+    </nav>
+    <!-- / 導航欄 -->
+
+    <!-- 網站標頭 -->
+    <header class="header">
+        <div class="header__title">友一表實本為性媽麼飯建他</div>
+        <div class="header__subtitle">大且容的南一起的現什除育通原上黑此不兒簡品心給舉那；白北代。</div>
+    </header  >
+    <!-- / 網站標頭 -->
+
+    <!-- 主要區塊 -->
+    <section class="section">
+        <!-- 單篇文章 -->
+        <article class="section__article">
+            <!-- 標題 -->
+            <header class="article__header">媽麼飯建他我反界相上最時小一名</header>
+            <!-- / 標題 -->
+
+            <!-- 內容預覽 -->
+            <section class="article__content">
+                <img class="content__image" src="assets/images/16-9.png">
+                記獨一學在個結去認，落底便清本利後了必對看就不，白營我交增辦工子驚全機雨年會童他壓：本步我況了讀牛本失命還，全社女手分友一表實本為性媽麼飯建他我反界相上最時小一名地才自樣分自應交……了路我光能孩時年整要卻……能年導康兒見朋慢，起現每技！香教停禮易面一運國為？聯團裡地；始久上、著理哥、真家是……破們到開有有氣問裡面問拿生家連市是……我先座得資找吃分感了雄習；大且容的南一起的現什除育通原上黑此不兒簡品心給舉那；白北代。的書發團了得運運。空子成片所南書傷沒多我可告傷再者大木
+            </section>
+            <!-- / 內容預覽 -->
+
+            <!-- 中繼資料 -->
+            <aside class="article__meta">
+                <!-- 文章標籤 -->
+                <div class="meta__section meta__section--primary">
+                    <span class="section__label">書發團了</span>
+                    <span class="section__label">容的南一起的</span>
+                    <span class="section__label">教停</span>
+                </div>
+                <!-- / 文章標籤 -->
+
+                <!-- 發表日期 -->
+                <time class="meta__section">
+                    2018 年 07 月 24 日
+                </time>
+                <!-- / 發表日期 -->
+
+                <!-- 發表者頭像 -->
+                <a rel="author" class="meta__section">
+                    <img class="section__author" src="assets/images/user.png">
+                </a>
+                <!-- / 發表者頭像 -->
+            </aside>
+            <!-- / 中繼資料 -->
+        </article>
+        <!-- / 單篇文章 -->
+    </section>
+    <!-- / 主要區塊 -->
+
+    <!-- 頁腳清單 -->
+    <footer>
+        <!-- 左側 -->
+        <div>
+            <!-- 單個小工具 -->
+            <div>
+
+            </div>
+            <!-- / 單個小工具 -->
+        </div>
+        <!-- / 左側 -->
+
+        <!-- 中間 -->
+        <div></div>
+        <!-- / 中間 -->
+
+        <!-- 右側 -->
+        <div></div>
+        <!-- / 右側 -->
+    </footer>
+    <!-- / 頁腳清單 -->
+</body>
+</html>
